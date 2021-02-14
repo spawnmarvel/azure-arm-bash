1f9fca2eb6fd755401094780bd712f8c952b88e2
espen <espenkleivane@gmail.com>
2021-02-14 21:12:16 +0100

    admin and files 2

--- learn-bash/1_files.sh
+++ learn-bash/1_files.sh
@@ -1,5 +1,5 @@
-# be root
-sudo /bin/su -
+cp --help
+cp file.txt new_file.txt
 # follow the log file
 tail -f log
 # check size
@@ -13,7 +13,3 @@ rm logstash-plain-2019*
 rm -f logstash-plain-2020-0[1]
 # remove all files with within april/may 2020
 rm -f logstash-plain-2020-0[34]
-#Install and remove git
-sudo apt install git-all
-sudo apt-get remove git-all
-sudo apt install git
---
+++ learn-bash/2_admin.sh
@@ -0,0 +1,6 @@
+# be root
+sudo /bin/su -
+#Install and remove git
+sudo apt install git-all
+sudo apt-get remove git-all
+sudo apt install git
