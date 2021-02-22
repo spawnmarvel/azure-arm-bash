2bb3e21a038be182e4ddaae0942d0d49a31f4606
espen <espenkleivane@gmail.com>
2021-02-22 21:38:20 +0100

    simple bck

---
+++ bash-snippets/4_hellow_world.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# https://linuxconfig.org/bash-scripting-tutorial-for-beginners
+
+echo "Hello world"
+pwd
---
+++ bash-snippets/5_simple_backup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# https://linuxconfig.org/bash-scripting-tutorial-for-beginners
+
+echo "Performing bck"
+tar -czf /home/espen/test_bck.tag.gz /home/espen/test
+echo "Backup done"
